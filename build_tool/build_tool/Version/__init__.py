59ab7e743806eb8f05bd639a579591f9ff3c578a
Shainnif Ismail <shaine@carsift.co>
2015-11-04 14:04:20 +0000

    more moving around

---
+++ build_tool/build_tool/Version/__init__.py
@@ -0,0 +1,48 @@
+import ConfigParser
+
+BUGFIX = "bugfix"
+MAJOR = "major"
+MINOR = "minor"
+
+
+def get_software_version_string(major, minor, bugfix):
+    return "{}.{}.{}".format(major, minor, bugfix)
+
+
+class Version(object):
+    """docstring for Version"""
+
+    def __init__(self, major, minor, bugfix):
+        self.major = major
+        self.minor = minor
+        self.bugfix = bugfix
+
+    @classmethod
+    def from_config_file(cls, config_path, section):
+        cls.config = ConfigParser.RawConfigParser()
+        cls.config.read(config_path)
+        return cls(cls.config.getint(section, MAJOR), cls.config.getint(section, MINOR),
+                   cls.config.getint(section, BUGFIX))
+
+    def get_next_version_string(self, revision):
+        if revision == MAJOR:
+            return self.next_major_version()
+        if revision == MINOR:
+            return self.next_minor_version()
+        if revision == BUGFIX:
+            return self.next_bug_fix_version
+
+    def next_bug_fix_version(self):
+        return get_software_version_string(self.major, self.minor, self.bugfix + 1)
+
+    def next_minor_version(self):
+        return get_software_version_string(self.major, self.minor + 1, self.bugfix)
+
+    def next_major_version(self):
+        return get_software_version_string(self.major + 1, self.minor, self.bugfix)
+
+    def get_current_version_string(self):
+        return get_software_version_string(self.major, self.minor, self.bugfix)
+
+    def add_new_version(self, bugfix):
+        self.config.add_section(get_software_version_string(self.major, self.minor, bugfix))
---
+++ build_tool/tests/build_tools_tests.py
@@ -0,0 +1,17 @@
+__author__ = 'shainnif'
+
+
+def setup():
+	print "SETUP!"
+
+
+def teardown():
+	print "TEAR DOWN!"
+
+
+def test_basic():
+	print "I RAN!"
+
+
+def test_basic2():
+	print "I RAN!"
